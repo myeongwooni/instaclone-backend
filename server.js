33ebb2f6299f63cafaa77be4cb7527aae2d0c802
myeongwooni <uriuni17@naver.com>
2021-06-03 04:49:16 +0900

    #2.4 User Module - File Upload, express

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-import {ApolloServer} from "apollo-server";
-import {typeDefs, resolvers} from "./schema.js";
+import express from "express";
+import logger from "morgan";
+import {ApolloServer} from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema.js";
 import { getUser } from "./users/users.utils.js";
 
 const server = new ApolloServer({
@@ -13,8 +15,10 @@ const server = new ApolloServer({
     },
 });
 
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({app});
 const PORT = process.env.PORT
-
-server
-    .listen(PORT)
-    .then(() => console.log(`💕 Server is running on http://localhost:${PORT} ❕`));
+app.listen({port:PORT}, () => {
+        console.log(`💕 Server is running on http://localhost:${PORT} ❕`);
+});
